2ae39c7731f7044aff74ab9690df66a18c163827
Isidora Milovanov <isidoramilovanov@gmail.com>
2018-03-01 21:39:25 +0100

    Modified Mario

--- Events/super_mario/mario.js
+++ Events/super_mario/mario.js
@@ -1,29 +1,32 @@
+var run1 = false;
 
 document.querySelector('img').setAttribute('src', './images/mario.png');
 var interval;
 function int (){
     var x = 0;
-        interval = setInterval(function (){
-            x+=1;
-            document.querySelector('body').style.backgroundPosition =  x + 'px 494px' ;
-        }, 50);
-        console.log(interval);
+    interval = setInterval(function (){
+        x+=1;
+        document.querySelector('body').style.backgroundPosition =  x + 'px 494px' ;
+    }, 30);
+    
 }
 
-document.querySelector('body').addEventListener("keydown", run);
 
+document.querySelector('body').addEventListener("keydown", run);
 function run(event){
-    if(event.keyCode == 39) {
+    if(event.keyCode == 39 && run1 == false) {
         document.querySelector('img').setAttribute('src', './images/mario_running.gif');
-       int();
+        int();
+        run1 = true;
     }
 };
 
 document.querySelector('body').addEventListener('keyup', stop);
-
 function stop(event){
+    if(run1 == true){
     document.querySelector('img').setAttribute('src', './images/mario.png');
-    console.log("izasli sa vrednoscu:" + interval);
     clearInterval(interval);
-    document.querySelector('body').style.backgroundPosition = '0px 494px' ;
+    run1 = false;
+    
+    }
 }
