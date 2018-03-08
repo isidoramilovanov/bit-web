e32e78bcb0608fb6dfb2612674f8edfde716d353
BIT Student <classroom@bgit.rs>
2018-03-08 17:53:36 +0100

    githubsearcher

---
+++ xml/gitHubSearcher/github.js
@@ -0,0 +1,25 @@
+$('input').keydown(function (event) {
+
+    if (event.keyCode === 13) {
+        var username = $('input').val();
+
+
+        var request = $.ajax({
+            url: `https://api.github.com/search/users?q=${username}`,
+            method: "GET",
+        });
+        request.done(function (msg) {
+            for (var i = 0; i < 7; i++) {
+                var avatar = msg.items[i].avatar_url;
+                var name = msg.items[i].login;
+                $(`p:eq(${i})`).text(name);
+                $(`img:eq(${i})`).attr("src", avatar);
+
+            }
+        });
+        request.fail(function (jqXHR, textStatus) {
+            alert("Request failed: " + textStatus);
+        });
+
+    }
+})
